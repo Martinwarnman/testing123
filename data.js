40beea3b4619236bceb1288c31db8dfa1af70420
Martinwarnman <martin.warnman@gmail.com>
2019-08-22 13:57:59 +0300

    update

---
+++ data.js
@@ -0,0 +1,32 @@
+var sqlite3 = require('sqlite3').verbose()
+
+const DBSOURCE = "games.db" 
+
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQlite database.')
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            email text UNIQUE, 
+            password text, 
+            CONSTRAINT email_unique UNIQUE (email)
+            )`,(err) => {
+        if (err) {
+            // Table already created
+        }else{
+            // Table just created, creating some rows
+            
+        }
+    })  
+    }
+})
+
+
+module.exports = db
+
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+const excelToJson = require('convert-excel-to-json');
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('games.db');
+const result = excelToJson({
+    sourceFile: 'ex.xls'
+});
+//console.log(result);
+
+//console.log(result.Sheet1[0].A);
+//FUNKAR console.log(result.k1[0].A);
+//console.log(result.k1[0].A, result.k1[0].B, result.k1[0].C);
+for (var i = 0; i < 10; i++) {
+    console.log(
+        result.k1[i].A + " : " +
+        result.k1[i].B + " : " +
+        result.k1[i].C + " : " +
+        result.k1[i].D + " : " +
+        result.k1[i].E + " : " +
+        result.k1[i].F + " : " +
+        result.k1[i].G + " : " +
+        result.k1[i].H + " : " +
+        result.k1[i].I
+    );
+}
+//console.log(result.k1);
+var sql = "INSERT INTO games(game) VALUES(" + result.k1[i].A + ")";
+
+// insert one row into the langs table
+db.run(`INSERT INTO games(game) VALUES(?,?)`, [result.k1[i].A,], function (err) {
+    if (err) {
+        return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+});
+
+// close the database connection
+db.close();
